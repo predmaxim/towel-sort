c2c38fadad2db61cf5580a00d03fd4af3b60d0c0
predmaxim <predmaxim@ya.ru>
2022-08-03 19:42:50 +0500

    feat: refacoring, optimize code

--- src/index.js
+++ src/index.js
@@ -1,21 +1,18 @@
 module.exports = function towelSort(matrix) {
   const res = []
-  const isEven = (n) => n % 2 == 0
 
-  if (matrix && matrix.length) {
+  if (matrix) {
+
     matrix.forEach((el, index) => {
+      index % 2 === 0 ? el.sort((a, b) => a - b) : el.sort((a, b) => b - a)
 
-      if (el.length) {
-
-        isEven(index) ? el.sort((a, b) => a - b) : el.sort((a, b) => b - a)
-
-        for (i = 0; i < el.length; i++) {
-          res.push(el[i])
-        }
-
+      for (i = 0; i < el.length; i++) {
+        res.push(el[i])
       }
+
     })
+
   }
-  
+
   return res
 }
